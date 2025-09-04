294281713bd8fa981baf3cdf29e683a20be5ef2f
viske13141 <viske13141@gmail.com>
2025-09-04 21:42:51 +0530

    Removed root home view to show default Django 404

--- backend/urls.py
+++ backend/urls.py
@@ -2,10 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from core import views
 
 urlpatterns = [
-    path("", views.home, name="home"),   # 👈 root URL
     path("admin/", admin.site.urls),
     path("api/", include("core.urls")),
 ]
